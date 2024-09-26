d1937999332951ea896a94dc88493fcb3289b155
conny <conny@connyui-Macmini.local>
2024-09-26 22:13:38 +0900

    add side, info bar

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,5 +1,6 @@
 import React from "react"
-
+import Sidebar from "@/components/sidebar"
+import InfoBar from "@/components/infobar"
 type Props = {
     children: React.ReactNode
 }
@@ -7,7 +8,11 @@ type Props = {
 const Layout = (props: Props) => {
     return (
         <div className="flex h-screen overflow-hidden">
-            <div className="w-full">{props.children}</div>
+            <Sidebar />
+            <div className="w-full">
+                <InfoBar />
+                {props.children}
+            </div>
         </div>
     )
 }
